c9785223e0e09b25cb63e0bf2acf05b405d9e369
efrain <efarinva98@gmail.com>
2020-04-03 21:15:01 -0400

    be fields

--- database/migrations/20191226143741_users.js
+++ database/migrations/20191226143741_users.js
@@ -1,11 +1,10 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("users", tbl => {
     tbl.increments();
-    tbl.string('location')
-    tbl.integer('rooms')
-    tbl.integer('price')
-
-    
+    tbl.string('Frist name')
+    tbl.integer('Last name')
+    tbl.integer('Email')
+    tbl.bool('House')
   });
 };
 
